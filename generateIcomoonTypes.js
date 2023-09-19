219a291bf68e48aad03a9157f86f96f9e47b5c71
Evan Šanta <code.selftaught@gmail.com>
2023-09-19 13:48:25 +0200

    Fix output file path

--- generateIcomoonTypes.js
+++ generateIcomoonTypes.js
@@ -7,9 +7,6 @@ const args = process.argv.slice(2);
 const inputFlag = args.indexOf('-i');
 const inputFileArg = args[inputFlag + 1];
 const inputFilePath = path.resolve(inputFileArg);
-const inputFilePath2 = path.resolve(process.cwd(), inputFileArg);
-
-console.log('inputFilePath', inputFilePath, inputFilePath2);
 
 const selection = JSON.parse(
   fs.readFileSync(
@@ -20,7 +17,7 @@ const selection = JSON.parse(
 const iconNames = selection.icons.map(icon => icon.properties.name);
 
 fs.writeFileSync(
-  './src/components/base/icons/icomoon.ts',
+  './src/components/base/icons/icomoon/icomoon.ts',
   `export const iconNames = ${JSON.stringify(
     iconNames,
   )} as const \nexport type IconName = (typeof iconNames)[number]`,
