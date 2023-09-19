22b807edd1b0c38749e438732c1cd219021917a8
Evan Šanta <code.selftaught@gmail.com>
2023-09-19 13:15:09 +0200

    Make generateIcommonTypes executable from parent

--- generateIcomoonTypes.js
+++ generateIcomoonTypes.js
@@ -1,7 +1,16 @@
+#!/usr/bin/env node
 const fs = require('fs');
 
+const args = process.argv.slice(2);
+
+const inputFlag = args.indexOf('-i');
+const inputFileArg = args[inputFlag + 1];
+
 const selection = JSON.parse(
-  fs.readFileSync('./public/fonts/icomoon/selection.json', 'utf8'),
+  fs.readFileSync(
+    inputFileArg || './public/fonts/icomoon/selection.json',
+    'utf8',
+  ),
 );
 const iconNames = selection.icons.map(icon => icon.properties.name);
 
