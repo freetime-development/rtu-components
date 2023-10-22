8ee38b31ec9728b750d6b783fc00d6067841506f
Evan Šanta <code.selftaught@gmail.com>
2023-10-22 16:37:50 +0200

    Update: Formatting Dates

--- src/utils/formatting.ts
+++ src/utils/formatting.ts
@@ -1,3 +1,7 @@
+import { DateTime } from 'luxon';
+
+const DEFAULT_DATE_FORMAT = 'yyyy-MM-dd';
+
 export function formatNum(
   number: number | bigint,
   options?: Intl.NumberFormatOptions,
@@ -21,4 +25,23 @@ export function formatCurrency(
     },
     locales,
   );
+}
+
+export function formatDate(
+  date: string | Date | number,
+  format: string = DEFAULT_DATE_FORMAT,
+) {
+  if (typeof date === 'number') {
+    return DateTime.fromMillis(date).toFormat(format);
+  }
+
+  if (date instanceof Date) {
+    return DateTime.fromJSDate(date).toFormat(format);
+  }
+
+  if (typeof date === 'string') {
+    return DateTime.fromISO(date).toFormat(format);
+  }
+
+  return date;
 }
