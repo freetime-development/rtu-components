30c742bdb3ddb15c4065e338c7359d2268f86cbf
Evan Šanta <code.selftaught@gmail.com>
2023-10-25 01:06:38 +0200

    Update: Select

--- src/components/composed/select/Select.tsx
+++ src/components/composed/select/Select.tsx
@@ -63,6 +63,7 @@ export function Select<O extends Option>({
   const errorMessage = validation?.errorMessage;
   const error = useFormError(name, errorMessage);
   const { field } = useController({ name, rules, defaultValue });
+  const value = options.find(o => o.value === field.value);
   const { filteredOptions, setQuery, clear } = useSelect<O>(
     name,
     initialQuery,
@@ -100,7 +101,7 @@ export function Select<O extends Option>({
         error={error}
         placeholder={placeholder}
         options={filteredOptions}
-        value={!field.value ? '' : String(field.value)}
+        value={!value ? '' : String(value)}
         onChange={handleOnChange}
         setQuery={setQuery}
         clear={clear}
