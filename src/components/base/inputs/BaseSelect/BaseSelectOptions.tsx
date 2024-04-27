b1d7d2b2a984e48f175ee5108d42dab3d8e51a0f
Evan Šanta <code.selftaught@gmail.com>
2024-04-27 05:28:25 +0200

    Fix typo

--- src/components/base/inputs/BaseSelect/BaseSelectOptions.tsx
+++ src/components/base/inputs/BaseSelect/BaseSelectOptions.tsx
@@ -36,7 +36,7 @@ export function BaseSelectOptions<O extends Option>({
         static
         className={twMerge(
           'transition-height absolute z-10 w-full overflow-auto border rounded-b-lg bg-white border-gray-200 overscroll-contain',
-          open ? 'max-h-60 rounded-t-none border-t-0' : 'max-h-0p',
+          open ? 'max-h-60 rounded-t-none border-t-0' : 'max-h-0',
           optionsClassName,
         )}
         style={{ transitionDuration: `${transitionDuration * 2}ms` }}
